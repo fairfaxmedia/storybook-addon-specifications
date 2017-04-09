630661229c56df6e2b17d8cb7e9a99566bdaacf6
Jason Darwin <jason.darwin@fairfaxmedia.co.nz>
2017-04-10 11:23:31 +1200

    Add sync and async tests

--- .storybook/__tests__/sample.ci.mocha.stories.js
+++ .storybook/__tests__/sample.ci.mocha.stories.js
@@ -1,20 +1,28 @@
 import React from "react";
-import {mount} from "enzyme";
+import {mount, shallow} from "enzyme";
+import chai from "chai";
+import chaiEnzyme from "chai-enzyme";
 import expect from "expect";
+import sinon from "sinon";
 
 const stories = storiesOf('Button - CI MOCHA Sample', module);
 
-stories.add('Hello World', function () {
-  const helloWorldStory =
-    <button onClick={action('Hello World')}>
-      Hello World
-    </button>;
+chai.use(chaiEnzyme());
 
-  specs(() => describe('Hello World', function () {
-    let output;
+const Components = {
+  Hello: function(props) {
+    return <button onClick={action('Hello World')} className={'button'}>
+      {props.text}
+    </button>;
+  }
+};
+
+stories.add('The Hello button', function () {
+  let output;
+  specs(() => describe('The Hello button', function () {
     beforeEach(function() {
       console.log('BEFORE EACH');
-      output = mount(helloWorldStory);
+      output = mount(<Components.Hello text={'Hello World'} />);
     });
 
     before(function() {
@@ -23,36 +31,45 @@ stories.add('Hello World', function () {
 
     afterEach(function() {
       console.log('AFTER EACH');
+      output.unmount();
     });
 
     after(function() {
       console.log('AFTER ');
     });
 
-    it('Should have the Hello World label', function () {
-      expect(output.text()).toContain('Hello World');
+    it('Should have the Hello World text during a synchronous test', function () {
+      expect(output.text()).toEqual('Hello World');
     });
 
-    it('Should have the Hello World label', function () {
-      expect(output.text()).toContain('Hello World');
+    it('Should have the Hello World text during an asynchronous promise test', function () {
+      return new Promise(function (resolve) {
+        setTimeout(function() {
+          expect(output.text()).toContain('Hello World');
+          resolve();
+        }, 200)
+      })
     });
 
+    it('Should have the Hello World text during an asynchronous done test', function (done) {
+      setTimeout(function() {
+        expect(output.text()).toContain('Hello World');
+        done();
+      }, 200)
+    });
   }));
-  return helloWorldStory;
+
+  return output;
 });
 
 stories.add('Hello Earth', function () {
-  const helloEarthStory =
-    <button onClick={action('Hello Earth')}>
-      Hello Earth
-    </button>;
-
+  let output;
   specs(() => describe('Hello Earth', function () {
-    it('Should have the Hello Earth label', function () {
-      let output = mount(helloEarthStory);
+    it('Should have the Hello Earth label during a synchronous test', function () {
+      output = mount(<Components.Hello text={'Hello Earth'} />);
       expect(output.text()).toContain('Hello Earth');
     });
   }));
 
-  return helloEarthStory;
+  return output;
 });
